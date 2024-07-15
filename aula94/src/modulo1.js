54341ac523b33e3c154c2a7e64ef7a126e93d261
Gabriel Silva <gabriel.oliviera.silva@gmail.com>
2024-07-15 17:58:16 -0300

    Criacao do modelos dos projetos seguintes

---
+++ aula94/src/modulo1.js
@@ -0,0 +1,14 @@
+export const nome = 'Gabriel';
+export const sobrenome = 'Silva';
+export const idade = 21;
+
+export function soma(x, y) {
+    return x + y;
+}
+
+export default class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}

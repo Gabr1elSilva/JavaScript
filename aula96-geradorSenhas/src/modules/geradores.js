bac78ff1fc49d1dc8ef3d8260c381ebdedba0716
Gabriel Silva <gabriel.oliviera.silva@gmail.com>
2024-07-16 21:55:55 -0300

    Inicio do exercicio de gerador de senha

---
+++ aula96-geradorSenhas/src/modules/geradores.js
@@ -0,0 +1,10 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const geraMaiuscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumero = () => String.fromCharCode(rand(48, 58));
+const simbolos = ',.[]{}!@#$%*()_-+=';
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
+
+function geraSenha(qtd, maiusculas, minusculas, numeros, simbolos) {
+
+}
